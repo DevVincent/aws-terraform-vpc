e2f762471176bac14c9c5a8040d7bd3e6ad9f349
Vicente Manzano <vicente.manzano@firemind.io>
2021-11-04 14:39:59 +0000

    adding files

--- vpc.tf
+++ vpc.tf
@@ -10,47 +10,47 @@ resource "aws_vpc" "vpc" {
   }
 }
 
-# Terraform Training Subnets
-resource "aws_subnet" "public-1" {
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = var.subnet_cidr["fe1"]
-  map_public_ip_on_launch = "true"
-  availability_zone       = "eu-west-1a"
-
-  tags = {
-    Name = "public-1"
-  }
-}
-
-resource "aws_subnet" "public-2" {
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = var.subnet_cidr["fe2"]
-  map_public_ip_on_launch = "true"
-  availability_zone       = "eu-west-1b"
-
-  tags = {
-    Name = "public-2"
-  }
-}
-
-resource "aws_subnet" "private-1" {
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = var.subnet_cidr["be1"]
-  map_public_ip_on_launch = "false"
-  availability_zone       = "eu-west-1a"
-
-  tags = {
-    Name = "private-1"
-  }
-}
-
-resource "aws_subnet" "private-2" {
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = var.subnet_cidr["be2"]
-  map_public_ip_on_launch = "false"
-  availability_zone       = "eu-west-1b"
-
-  tags = {
-    Name = "private-2"
-  }
-}
+//# Terraform Training Subnets
+//resource "aws_subnet" "public-1" {
+//  vpc_id                  = aws_vpc.vpc.id
+//  cidr_block              = var.subnet_cidr["fe1"]
+//  map_public_ip_on_launch = "true"
+//  availability_zone       = "eu-west-1a"
+//
+//  tags = {
+//    Name = "public-1"
+//  }
+//}
+//
+//resource "aws_subnet" "public-2" {
+//  vpc_id                  = aws_vpc.vpc.id
+//  cidr_block              = var.subnet_cidr["fe2"]
+//  map_public_ip_on_launch = "true"
+//  availability_zone       = "eu-west-1b"
+//
+//  tags = {
+//    Name = "public-2"
+//  }
+//}
+//
+//resource "aws_subnet" "private-1" {
+//  vpc_id                  = aws_vpc.vpc.id
+//  cidr_block              = var.subnet_cidr["be1"]
+//  map_public_ip_on_launch = "false"
+//  availability_zone       = "eu-west-1a"
+//
+//  tags = {
+//    Name = "private-1"
+//  }
+//}
+//
+//resource "aws_subnet" "private-2" {
+//  vpc_id                  = aws_vpc.vpc.id
+//  cidr_block              = var.subnet_cidr["be2"]
+//  map_public_ip_on_launch = "false"
+//  availability_zone       = "eu-west-1b"
+//
+//  tags = {
+//    Name = "private-2"
+//  }
+//}
